c7162ef46bf87bef57362e8c30c373cf4b3cafc0
rushoptimus <mukeshgoyal573@gmail.com>
2025-08-09 19:59:39 +0530

    newsss

--- Backend/utils/generateTokenAndSetCookie.js
+++ Backend/utils/generateTokenAndSetCookie.js
@@ -7,7 +7,7 @@ export const generateTokenAndSetCookie = (res,userID) =>{
     res.cookie("token",token,{
         httpOnly:true,
         secure: process.env.NODE_ENV==="production",
-        sameSite:"strict",
+        sameSite:"none",
         maxAge: 1*24*60*60*1000,
         
     });
