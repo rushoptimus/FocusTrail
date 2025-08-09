6589493416e3d96914031b396059b28cc4b07d37
rushoptimus <mukeshgoyal573@gmail.com>
2025-08-09 22:12:08 +0530

    done

--- Backend/middlewares/verifyToken.js
+++ Backend/middlewares/verifyToken.js
@@ -1,7 +1,8 @@
 import jsonwebtoken from "jsonwebtoken";
 
 export const verifyToken = (req, res, next) => {
-    const token = req.cookies.token ;
+  const token = req.cookies?.token;
+
     try{
         if (!token) {
             return res.status(401).json({success:false, message: "Unauthorized" });
